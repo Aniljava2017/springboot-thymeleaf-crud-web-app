ea6e89d7c129d8c1e39656a2bac77bb806f53e6f
hp <anil.java2017@gmail.com>
2021-06-30 23:56:37 +0530

    Employee CRUD Admin Page

--- src/main/java/com/example/springbootthymeleafcrudwebapp/controller/EmployeeController.java
+++ src/main/java/com/example/springbootthymeleafcrudwebapp/controller/EmployeeController.java
@@ -1,10 +1,15 @@
 package com.example.springbootthymeleafcrudwebapp.controller;
 
+import com.example.springbootthymeleafcrudwebapp.model.Employee;
 import com.example.springbootthymeleafcrudwebapp.service.EmployeeService;
+import javassist.bytecode.ClassFile;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Controller
@@ -13,9 +18,77 @@ public class EmployeeController {
     EmployeeService employeeService;
     // Display list of employee
     @GetMapping("/")
-
-    public String viewHomePage(Model model){
-        model.addAttribute("listEmployee", employeeService.getAllEmployee());
-        return "index";
+    public String viewHomePage(Model model) {
+        return findPaginated(1, "firstName", "asc", model);
     }
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model){
+        // Create model attribute to bind form data
+        Employee employee = new Employee();
+        model.addAttribute("employee",employee);
+        return "new_employee";
+    }
+    @PostMapping("/saveEmployee")
+    // Save Employee to database
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+    @GetMapping("/showFormFormUpdate/{id}")
+    public String showFormFormUpdate(@PathVariable (value = "id") long id, Model model){
+    // get employee from the service
+        Employee employee = employeeService.getEmployeeById(id);
+
+        // set employee as a model attribute to pre-populated the form
+        model.addAttribute("employee",employee);
+        return "update_employee";
+    }
+    @GetMapping("/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable (value = "id") long id){
+        // call delete employee methode
+        this.employeeService.deleteEmployeeById(id);
+        return "redirect:/";
+    }
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable (value = "pageNo") int pageNo ,
+                                @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir,
+                                Model model){
+        int pageSize = 5;
+        Page<Employee> page = employeeService.findPaginated(pageNo,pageSize,sortField,sortDir);
+        List<Employee> listEmployees = page.getContent();
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages",page.getTotalPages());
+        model.addAttribute("totalItems",page.getTotalElements());
+
+        model.addAttribute("sortField",sortField);
+        model.addAttribute("sortDir",sortDir);
+        model.addAttribute("reverseSortDir",sortDir.equals("asc") ? "desc" : "asc");
+
+        model.addAttribute("listEmployees",listEmployees);
+        return "index";
+        //return "redirect:/";
+    }
+   /* @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo,
+                                @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir,
+                                Model model) {
+        int pageSize = 5;
+
+        Page < Employee > page = employeeService.findPaginated(pageNo, pageSize, sortField, sortDir);
+        List < Employee > listEmployees = page.getContent();
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
+        model.addAttribute("listEmployees", listEmployees);
+        return "index";
+    }*/
 }
--- src/main/java/com/example/springbootthymeleafcrudwebapp/service/EmployeeService.java
+++ src/main/java/com/example/springbootthymeleafcrudwebapp/service/EmployeeService.java
@@ -2,10 +2,16 @@ package com.example.springbootthymeleafcrudwebapp.service;
 
 import com.example.springbootthymeleafcrudwebapp.model.Employee;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public interface EmployeeService {
     List<Employee> getAllEmployee();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+    Page<Employee> findPaginated(int pageNo, int pageSize);
+    Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
 }
